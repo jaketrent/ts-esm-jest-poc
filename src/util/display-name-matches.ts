b20e4420aba690f5714395f667b9b56992abe1e3
jaketrent <trent.jake@gmail.com>
2021-03-19 16:07:43 -0600

    pare down util

--- src/util/display-name-matches.ts
+++
@@ -1,8 +0,0 @@
-import { ForwardRefExoticComponent, NamedExoticComponent, FC } from 'react'
-
-export function displayNameMatches(
-  instance: any,
-  el: NamedExoticComponent | FC | ForwardRefExoticComponent<any>
-) {
-  return instance && instance?.type?.displayName === el.displayName
-}
