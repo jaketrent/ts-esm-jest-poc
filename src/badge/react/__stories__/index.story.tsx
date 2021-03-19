1aae65b6d865b25f8754f9596057d51e9cece7e0
jaketrent <trent.jake@gmail.com>
2021-03-19 15:58:12 -0600

    cp ds packages

---
+++ src/badge/react/__stories__/index.story.tsx
@@ -0,0 +1,28 @@
+import { ValueOf } from '@pluralsight/ps-design-system-util'
+import { storiesOf } from '@storybook/react'
+
+import React from 'react'
+
+import Badge from '..'
+import * as vars from '../../vars'
+
+const BadgeStory = ({
+  appearance
+}: {
+  appearance: ValueOf<typeof vars.appearances>
+}) => (
+  <div style={{ display: 'grid', gridGap: 10 }}>
+    {Object.values(Badge.colors).map((color, i) => (
+      <div key={i}>
+        <Badge appearance={appearance} color={color}>
+          {appearance} {color}
+        </Badge>
+      </div>
+    ))}
+  </div>
+)
+
+const appearanceStory = storiesOf('appearance', module)
+Object.values(Badge.appearances).forEach(appearance =>
+  appearanceStory.add(appearance, () => <BadgeStory appearance={appearance} />)
+)
---
+++ src/badge/react/index.tsx
@@ -0,0 +1,75 @@
+import Theme, { useTheme } from '@pluralsight/ps-design-system-theme'
+import {
+  HTMLPropsFor,
+  RefForwardingComponent,
+  ValueOf
+} from '@pluralsight/ps-design-system-util'
+import { css, StyleAttribute } from 'glamor'
+import React from 'react'
+
+import stylesheet from '../css'
+import { select } from '../js'
+import * as vars from '../vars'
+
+type StyleFn = (
+  props: InternalBadgeProps,
+  themeName: ValueOf<typeof Theme.names>
+) => StyleAttribute
+
+const styles: { [key: string]: StyleFn } = {
+  badge: (props, themeName) =>
+    css(
+      stylesheet['.psds-badge'],
+      stylesheet[select(themeName, props.appearance, props.color)]
+    )
+}
+
+interface InternalBadgeProps {
+  appearance: ValueOf<typeof vars.appearances>
+  color: ValueOf<typeof vars.colors>
+}
+
+type BadgeProps = HTMLPropsFor<'div'> & Partial<InternalBadgeProps>
+
+interface BadgeStatics {
+  appearances: typeof vars.appearances
+  colors: typeof vars.colors
+}
+
+interface BadgeComponent
+  extends RefForwardingComponent<BadgeProps, HTMLDivElement, BadgeStatics> {}
+
+const Badge = React.forwardRef<HTMLDivElement, BadgeProps>(
+  (
+    {
+      appearance = vars.appearances.default,
+      color = vars.colors.neutral,
+      ...rest
+    },
+    forwardedRef
+  ) => {
+    const ref = React.useRef<HTMLDivElement>(null)
+    // TODO: consider replacing with useCombinedRefs
+    React.useImperativeHandle(
+      forwardedRef,
+      () => (ref.current as unknown) as HTMLDivElement
+    )
+    const themeName = useTheme()
+
+    return (
+      <div
+        {...styles.badge({ appearance, color }, themeName)}
+        ref={ref}
+        {...rest}
+      />
+    )
+  }
+) as BadgeComponent
+
+Badge.appearances = vars.appearances
+Badge.colors = vars.colors
+
+export const appearances = vars.appearances
+export const colors = vars.colors
+
+export default Badge
